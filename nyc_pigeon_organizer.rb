617c1da6e03cd0b6136ffb0e454465b0993e0712
Ana Robalo <anameciarobalo@gmail.com>
2019-11-17 20:30:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_hash = {}
+  data.each do |trait,attribute|
+    attribute.each do |detail,names|
+      names.each do |name|
+        if !pigeon_hash[name] 
+          pigeon_hash[name] = {}
+        end
+        if !pigeon_hash[name][trait]
+          pigeon_hash[name][trait] = []
+        end
+        pigeon_hash[name][trait].push(detail.to_s)
+      end
+    end
+  end
+  pigeon_hash
 end
